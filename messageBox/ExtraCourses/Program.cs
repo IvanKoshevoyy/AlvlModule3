1bc06eda69b96cb199167fe536c49be240d163c3
IvanKoshyvoyy <ivankoshevoy2@gmail.com>
2022-01-13 18:42:36 +0200

    28.12.2021 MessageBoxPractise

---
+++ messageBox/ExtraCourses/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraCourses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox messageBox = new MessageBox();
+            messageBox.closedWindow += (State state) => 
+            {
+                if (state == 0)
+                    Console.WriteLine("operation was succeeded");
+                else
+                {
+                    Console.WriteLine("operation was cancelled");
+                }
+
+            };
+            messageBox.Open();
+            Console.ReadLine();
+        }
+
+
+    }
+}
